ce559350c10c6f92208c19525931717a5573beea
TEENOLA <adeoluwa.adenola@gmail.com>
2023-07-02 21:32:51 +0100

    adjusted clock hands

--- digital clock 3/jav2.js
+++ digital clock 3/jav2.js
@@ -3,20 +3,13 @@ let min =document.getElementById("min")
 let sec =document.getElementById("sec")
 let period = document.getElementById("period")
 
+setInterval(function(tellTime){
 
-setInterval(()=>{
-
-let currentTime = new Date();
-
-if (hrs >= 12){
-    period.innerHTML = 'PM'
-}else{
-    period.innerHTML = 'AM'
-}
-
+ let currentTime = new Date();   
 hrs.innerHTML = (currentTime.getHours()<10?"0":"")+ currentTime.getHours()
 min.innerHTML = (currentTime.getMinutes()<10?"0":"")+ currentTime.getMinutes()
 sec.innerHTML = (currentTime.getSeconds()<10?"0":"")+ currentTime.getSeconds()
+period.innerHTML = hrs >= 12 ? "PM" : "AM"; 
 
 },1000)
 
